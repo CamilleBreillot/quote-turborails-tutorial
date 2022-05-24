977a1ff6fb27ebf464a4ecced859b762732c700f
camille breillot <camille.breillot@gmail.com>
2022-05-24 18:38:41 +0200

    association with current_company

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = current_company.quotes.ordered
+    @quotes = current_company.quotes
   end
 
   def show
